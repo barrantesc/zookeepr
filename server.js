2e0f8d689da71d20a9f26eb16a167433a09962b4
playground <connie.s.barrantes@gmail.com>
2022-01-21 05:53:41 -0500

    added a 404 error

--- server.js
+++ server.js
@@ -39,18 +39,39 @@ function filterByQuery(query, animalsArray) {
     if (query.name) {
       filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
-    // return the filtered results:
     return filteredResults;
-  }
+}
 
+// function takes id and array of animals & returns single animal object
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+// GET route
 app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+// param route must come after the get route
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  res.json(result);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
